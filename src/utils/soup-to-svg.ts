f808614212dcdea65c5252a2563ef8d7453bc432
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2024-07-16 20:59:30 +0530

    Update src/utils/soup-to-svg.ts
    Co-authored-by: Severin Ibarluzea <seve700@gmail.com>

--- src/utils/soup-to-svg.ts
+++ src/utils/soup-to-svg.ts
@@ -1,4 +1,6 @@
-export function soupToSvg(soup: any[]): string {
+import type { AnySoupElement } from "@tscircuit/soup"
+
+export function soupToSvg(soup: AnySoupElement[]): string {
   // TODO: Implement soupToSvg
   return "";
 }
