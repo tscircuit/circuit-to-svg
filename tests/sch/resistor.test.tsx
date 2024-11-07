264417c45acdbf9a8698f5794ab00d370e74927a
seveibar <seveibar@gmail.com>
2024-11-07 13:03:41 -0800

    switch to symbol_display_value for symbol values

--- tests/sch/resistor.test.tsx
+++ tests/sch/resistor.test.tsx
@@ -18,6 +18,19 @@ test("schematic resistor", () => {
 
   expect(
     // @ts-ignore
-    convertCircuitJsonToSchematicSvg(project.getCircuitJson()),
+    convertCircuitJsonToSchematicSvg(
+      project
+        .getCircuitJson()
+        // TEMPORARY HACK: until @tscircuit/core supports symbol_display_value
+        .map((elm) => {
+          if (elm.type === "schematic_component") {
+            return {
+              ...elm,
+              symbol_display_value: "10Ω",
+            }
+          }
+          return elm
+        }),
+    ),
   ).toMatchSvgSnapshot(import.meta.path)
 })
