88594b73fd4212e5e648e543305d2320e1a9131e
seveibar <seveibar@gmail.com>
2024-11-08 14:42:13 -0800

    wip reproduce issue

--- tests/sch/net-label.test.tsx
+++ tests/sch/net-label.test.tsx
@@ -4,11 +4,49 @@ import { convertCircuitJsonToSchematicSvg } from "lib/index"
 
 test("schematic net label", () => {
   const circuitJson: SchematicNetLabel[] = [
-    // TODO
+    {
+      type: "schematic_net_label",
+      source_net_id: "net1",
+      center: { x: 0, y: 0 },
+      anchor_side: "top",
+      text: "VCC",
+    },
+    {
+      type: "schematic_net_label",
+      source_net_id: "net2",
+      center: { x: 4, y: 0 },
+      anchor_side: "bottom",
+      text: "GND",
+    },
+    {
+      type: "schematic_net_label",
+      source_net_id: "net3",
+      center: { x: 0, y: 4 },
+      anchor_side: "left",
+      text: "SDA",
+    },
+    {
+      type: "schematic_net_label",
+      source_net_id: "net4",
+      center: { x: 4, y: 4 },
+      anchor_side: "right",
+      text: "SCL",
+    },
   ]
 
   expect(
     // @ts-ignore
-    convertCircuitJsonToSchematicSvg(circuitJson),
+    convertCircuitJsonToSchematicSvg(circuitJson, {
+      grid: {
+        cellSize: 1,
+        labelCells: true,
+      },
+      labeledPoints: [
+        { x: 0, y: 0, label: "VCC" },
+        { x: 4, y: 0, label: "GND" },
+        { x: 0, y: 4, label: "SDA" },
+        { x: 4, y: 4, label: "SCL" },
+      ],
+    }),
   ).toMatchSvgSnapshot(import.meta.path)
 })
