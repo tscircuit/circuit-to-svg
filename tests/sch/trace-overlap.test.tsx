01d81285a495983941158492ada4c4d6116e8325
seveibar <seveibar@gmail.com>
2024-11-07 20:49:17 -0800

    format

--- tests/sch/trace-overlap.test.tsx
+++ tests/sch/trace-overlap.test.tsx
@@ -21,20 +21,14 @@ test("schematic trace overlap", () => {
         schX={-2}
         schY={-1}
       />
-      <resistor 
-        name="R3" 
-        resistance="10k" 
-        footprint="0402" 
-        schX={0} 
-        schY={2} 
-      />
+      <resistor name="R3" resistance="10k" footprint="0402" schX={0} schY={2} />
 
       <trace from=".R1 > .pin2" to=".R3 > .pin1" />
       <trace from=".R2 > .pin2" to=".R3 > .pin2" />
-    </board>
+    </board>,
   )
 
   expect(
-    convertCircuitJsonToSchematicSvg(project.getCircuitJson())
+    convertCircuitJsonToSchematicSvg(project.getCircuitJson()),
   ).toMatchSvgSnapshot(import.meta.path)
 })
