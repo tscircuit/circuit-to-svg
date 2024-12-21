e0b6f2c570b38b42e610790be1324a06cf07659f
Fahmi Akbar Wildana <sensorfied@gmail.com>
2024-12-21 18:50:22 +0700

    refactor copied hooks

--- lib/3d/svg-object-fns/use-stls-from-geom.ts
+++ lib/3d/svg-object-fns/use-stls-from-geom.ts
@@ -1,6 +1,5 @@
-import { useState, useEffect } from "react"
 import stlSerializer from "@jscad/stl-serializer"
-import { Geom3 } from "@jscad/modeling/src/geometries/types"
+import type { Geom3 } from "@jscad/modeling/src/geometries/types"
 
 function blobToBase64Url(blob: Blob): Promise<string> {
   return new Promise((resolve, reject) => {
@@ -15,41 +14,27 @@ function blobToBase64Url(blob: Blob): Promise<string> {
 
 type StlObj = { stlUrl: string; color: number[] }
 
-export const useStlsFromGeom = (
-  geom: Geom3[] | Geom3 | null,
-): {
-  stls: StlObj[]
-  loading: boolean
-} => {
-  const [stls, setStls] = useState<StlObj[]>([])
-  const [loading, setLoading] = useState(true)
+export const useStlsFromGeom = (geom: Geom3[] | Geom3): StlObj[] => {
+  let stls: StlObj[] = []
 
-  useEffect(() => {
-    if (!geom) return
-    const generateStls = async () => {
-      setLoading(true)
-      const geometries = Array.isArray(geom) ? geom : [geom]
+  const generateStls = async () => {
+    const geometries = Array.isArray(geom) ? geom : [geom]
 
-      const stlPromises = geometries.map(async (g) => {
-        const rawData = stlSerializer.serialize({ binary: true }, [g])
-        const blobData = new Blob(rawData)
-        const stlUrl = await blobToBase64Url(blobData)
-        return { stlUrl, color: g.color! }
-      })
+    const stlPromises = geometries.map(async (g) => {
+      const rawData = stlSerializer.serialize({ binary: true }, [g])
+      const blobData = new Blob(rawData)
+      const stlUrl = await blobToBase64Url(blobData)
+      return { stlUrl, color: g.color! }
+    })
 
-      try {
-        const generatedStls = await Promise.all(stlPromises)
-        setStls(generatedStls)
-      } catch (error) {
-        console.error("Error generating STLs:", error)
-        setStls([])
-      } finally {
-        setLoading(false)
-      }
+    try {
+      stls = await Promise.all(stlPromises)
+    } catch (error) {
+      console.error("Error generating STLs:", error)
     }
+  }
 
-    generateStls()
-  }, [geom])
+  generateStls()
 
-  return { stls, loading }
+  return stls
 }
