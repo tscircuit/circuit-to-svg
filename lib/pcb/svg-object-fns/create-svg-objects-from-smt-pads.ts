f7c6e15804e2c45b4b76e2b634001e682d447dc4
techmannih <manishchaudharyttt@gmail.com>
2025-01-11 09:46:01 +0530

    format code

--- lib/pcb/svg-object-fns/create-svg-objects-from-smt-pads.ts
+++ lib/pcb/svg-object-fns/create-svg-objects-from-smt-pads.ts
@@ -52,7 +52,6 @@ export function createSvgObjectsFromSmtPad(
     const height = pad.height * Math.abs(transform.d)
     const radius = pad.radius * Math.abs(transform.a)
 
-    // Create a pill shape (rectangular with rounded corners)
     return [
       {
         name: "rect",
@@ -64,8 +63,8 @@ export function createSvgObjectsFromSmtPad(
           y: (y - height / 2).toString(),
           width: width.toString(),
           height: height.toString(),
-          rx: radius.toString(), // Rounded corners for "pill" shape
-          ry: radius.toString(), // Rounded corners for "pill" shape
+          rx: radius.toString(),
+          ry: radius.toString(),
         },
       },
     ]
