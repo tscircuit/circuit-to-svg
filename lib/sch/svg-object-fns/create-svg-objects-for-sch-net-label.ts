8d6c167ed3bbf61142af8414325c2a6bcffc6045
techmannih <manishchaudharyttt@gmail.com>
2025-01-15 23:50:53 +0530

    update transparency

--- lib/sch/svg-object-fns/create-svg-objects-for-sch-net-label.ts
+++ lib/sch/svg-object-fns/create-svg-objects-for-sch-net-label.ts
@@ -150,7 +150,7 @@ export const createSvgObjectsForSchNetLabel = (
     attributes: {
       class: "net-label",
       d: pathD,
-      fill: "transparent",
+      fill: "#FFFFFF99",
       stroke: colorMap.schematic.label_global,
       "stroke-width": `${getSchStrokeSize(realToScreenTransform)}px`,
     },
