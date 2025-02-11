ec9ed6a3bc2f903472126588935d256ad2255d68
Arnav K <arnavkaushal09@gmail.com>
2025-02-11 18:53:49 +0000

    use su to get element pos

--- lib/pcb/create-svg-objects-from-pcb-rats-nest/get-element-position.ts
+++ lib/pcb/create-svg-objects-from-pcb-rats-nest/get-element-position.ts
@@ -1,19 +1,22 @@
 import type { AnyCircuitElement } from "circuit-json"
+import { su } from "@tscircuit/soup-util"
 
 export const getElementPosition = (
   id: string,
   circuitJson: AnyCircuitElement[],
 ): { x: number; y: number } | null => {
-  const elm = circuitJson.find((element) => {
-    const elmId =
-      (element as any).id ||
-      (element as any).pcb_port_id ||
-      (element as any).source_port_id
-    return elmId === id
-  })
-
-  if (elm && "x" in elm && "y" in elm) {
-    return { x: (elm as any).x, y: (elm as any).y }
+  // Try to find the element as a pcb_smtpad
+  const pcbSmtpad = su(circuitJson).pcb_smtpad.get(id)
+  if (pcbSmtpad && "x" in pcbSmtpad && "y" in pcbSmtpad) {
+    return { x: (pcbSmtpad as any).x, y: (pcbSmtpad as any).y }
   }
+
+  // Try to find the element as a pcb_plated_hole
+  const pcbPlatedHole = su(circuitJson).pcb_plated_hole.get(id)
+  if (pcbPlatedHole && "x" in pcbPlatedHole && "y" in pcbPlatedHole) {
+    return { x: (pcbPlatedHole as any).x, y: (pcbPlatedHole as any).y }
+  }
+
+  // If neither is found, return null
   return null
 }
