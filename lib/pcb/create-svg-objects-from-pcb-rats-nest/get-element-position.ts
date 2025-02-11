048ad858bfbcd2ebe1368d1912400baa96245669
Arnav K <arnavkaushal09@gmail.com>
2025-02-11 19:04:26 +0000

    remove the implicit typing to any I guess

--- lib/pcb/create-svg-objects-from-pcb-rats-nest/get-element-position.ts
+++ lib/pcb/create-svg-objects-from-pcb-rats-nest/get-element-position.ts
@@ -1,20 +1,25 @@
 import type { AnyCircuitElement } from "circuit-json"
 import { su } from "@tscircuit/soup-util"
 
+interface Position {
+  x: number
+  y: number
+}
+
 export const getElementPosition = (
   id: string,
   circuitJson: AnyCircuitElement[],
-): { x: number; y: number } | null => {
+): Position | null => {
   // Try to find the element as a pcb_smtpad
   const pcbSmtpad = su(circuitJson).pcb_smtpad.get(id)
   if (pcbSmtpad && "x" in pcbSmtpad && "y" in pcbSmtpad) {
-    return { x: (pcbSmtpad as any).x, y: (pcbSmtpad as any).y }
+    return { x: pcbSmtpad.x, y: pcbSmtpad.y }
   }
 
   // Try to find the element as a pcb_plated_hole
   const pcbPlatedHole = su(circuitJson).pcb_plated_hole.get(id)
   if (pcbPlatedHole && "x" in pcbPlatedHole && "y" in pcbPlatedHole) {
-    return { x: (pcbPlatedHole as any).x, y: (pcbPlatedHole as any).y }
+    return { x: pcbPlatedHole.x, y: pcbPlatedHole.y }
   }
 
   // If neither is found, return null
