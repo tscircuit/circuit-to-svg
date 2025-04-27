81a147bdd4ecabd76e6da6527f44e3fc29eedae7
Shibo <shibosoftwaredev@gmail.com>
2025-04-27 17:09:05 +0200

    draw bottom layer components with dashed lines (#202)
    * draw bottom layer components with dashed lines
    
    * rename
    
    * rename
    
    * fixed type error
    
    * used layerref type

--- tests/assembly/assembly-large-board.test.tsx
+++ tests/assembly/assembly-large-board.test.tsx
@@ -11,50 +11,40 @@ test("chip and resistor in assembly", () => {
 
   circuit.add(
     <board width="100mm" height="100mm">
-      {/* Create a 5x5 grid of resistors */}
-      {Array.from({ length: 5 }).map((_, row) =>
-        Array.from({ length: 5 }).map((_, col) => (
-          <resistor
-            key={`resistor-${row}-${col}`}
-            footprint="0402"
-            name={`R${row}${col}`}
-            resistance={1000 + (row * 5 + col) * 100}
-            pcbX={-40 + col * 20}
-            pcbY={-40 + row * 20}
-          />
-        )),
-      )}
+      {Array.from({ length: 2 }).map((_, col) => (
+        <resistor
+          key={`resistor-${col}`}
+          footprint="0402"
+          name={`R${col}`}
+          resistance={1000 + col * 100}
+          pcbX={-40 + col * 20}
+          pcbY={-40}
+        />
+      ))}
 
-      {/* Create a 5x5 grid of capacitors */}
-      {Array.from({ length: 5 }).map((_, row) =>
-        Array.from({ length: 5 }).map((_, col) => (
-          <capacitor
-            key={`capacitor-${row}-${col}`}
-            footprint="2512"
-            name={`C${row}${col}`}
-            capacitance={`${1 + (row * 5 + col)}uF`}
-            pcbX={-40 + col * 20}
-            pcbY={-40 + row * 20 + 10}
-          />
-        )),
-      )}
+      {Array.from({ length: 2 }).map((_, col) => (
+        <capacitor
+          key={`capacitor-${col}`}
+          footprint="2512"
+          name={`C${col}`}
+          capacitance={`${1 + col}uF`}
+          pcbX={-40 + col * 20}
+          pcbY={-40 + 10}
+        />
+      ))}
 
-      {/* Create a 5x5 grid of capacitors */}
-      {Array.from({ length: 5 }).map((_, row) =>
-        Array.from({ length: 5 }).map((_, col) => (
-          <capacitor
-            key={`capacitor-${row}-${col}`}
-            footprint="0805"
-            name={`C${row}${col}`}
-            capacitance={`${1 + (row * 5 + col)}uF`}
-            pcbX={-50 + col * 20}
-            pcbY={-50 + row * 20 + 10}
-          />
-        )),
-      )}
+      {Array.from({ length: 2 }).map((_, col) => (
+        <capacitor
+          key={`capacitor-${col}`}
+          footprint="0805"
+          name={`D${col}`}
+          capacitance={`${1 + col}uF`}
+          pcbX={-50 + col * 20}
+          pcbY={-50 + 10}
+        />
+      ))}
 
-      {/* Add 10 chips in a row */}
-      {Array.from({ length: 10 }).map((_, i) => (
+      {Array.from({ length: 2 }).map((_, i) => (
         <chip
           key={`chip-${i}`}
           name={`U${i}`}
@@ -73,12 +63,31 @@ test("chip and resistor in assembly", () => {
           pcbY={20}
         />
       ))}
+      {Array.from({ length: 2 }).map((_, i) => (
+        <chip
+          key={`chip-${i}`}
+          name={`E${i}`}
+          pinLabels={{
+            pin1: "D0",
+            pin2: "D1",
+            pin3: "D2",
+            pin4: "GND",
+            pin5: "D3",
+            pin6: "EN",
+            pin7: "D4",
+            pin8: "VCC",
+          }}
+          layer="bottom"
+          footprint="soic16"
+          pcbX={-20 + i * 10}
+          pcbY={20}
+        />
+      ))}
 
-      {/* Add 10 chips in a row */}
-      {Array.from({ length: 10 }).map((_, i) => (
+      {Array.from({ length: 2 }).map((_, i) => (
         <chip
           key={`chip-${i * 2}`}
-          name={`U${i * 2}`}
+          name={`F${i * 2}`}
           pinLabels={{
             pin1: "D0",
             pin2: "D1",
