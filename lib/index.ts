1c36e276932eb944104b9906a96f4233e9cff0f4
Shibo <shibosoftwaredev@gmail.com>
2025-05-02 14:52:45 +0200

    export colormap type

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
 export * from "./pcb/convert-circuit-json-to-pcb-svg"
 export * from "./assembly/convert-circuit-json-to-assembly-svg"
 export * from "./sch/convert-circuit-json-to-schematic-svg"
+export type { ColorMap } from "./utils/colors"
