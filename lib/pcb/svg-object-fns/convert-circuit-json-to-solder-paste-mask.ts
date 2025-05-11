7499bfd0745b0a25a7ef300a330bb4d49f24043d
techmannih <manishchaudharyttt@gmail.com>
2025-05-11 10:46:49 +0530

    add support solder paste for smtpads

---
+++ lib/pcb/svg-object-fns/convert-circuit-json-to-solder-paste-mask.ts
@@ -0,0 +1,88 @@
+import type { PcbSolderPaste } from "circuit-json"
+import { applyToPoint, type Matrix } from "transformation-matrix"
+import { solderPasteLayerNameToColor } from "../layer-name-to-color"
+
+export function createSvgObjectsFromSolderPaste(
+  solderPaste: PcbSolderPaste,
+  transform: Matrix,
+): any {
+  const [x, y] = applyToPoint(transform, [solderPaste.x, solderPaste.y])
+
+  if (solderPaste.shape === "rect" || solderPaste.shape === "rotated_rect") {
+    const width = solderPaste.width * Math.abs(transform.a)
+    const height = solderPaste.height * Math.abs(transform.d)
+
+    if (solderPaste.shape === "rotated_rect" && solderPaste.ccw_rotation) {
+      return [
+        {
+          name: "rect",
+          type: "element",
+          attributes: {
+            class: "pcb-solder-paste",
+            fill: solderPasteLayerNameToColor(solderPaste.layer),
+            x: (-width / 2).toString(),
+            y: (-height / 2).toString(),
+            width: width.toString(),
+            height: height.toString(),
+            transform: `translate(${x} ${y}) rotate(${-solderPaste.ccw_rotation})`,
+          },
+        },
+      ]
+    }
+
+    return [
+      {
+        name: "rect",
+        type: "element",
+        attributes: {
+          class: "pcb-solder-paste",
+          fill: solderPasteLayerNameToColor(solderPaste.layer),
+          x: (x - width / 2).toString(),
+          y: (y - height / 2).toString(),
+          width: width.toString(),
+          height: height.toString(),
+        },
+      },
+    ]
+  }
+  // Implement pill-shaped SMT pad
+  if (solderPaste.shape === "pill") {
+    const width = solderPaste.width * Math.abs(transform.a)
+    const height = solderPaste.height * Math.abs(transform.d)
+    const radius = solderPaste.radius * Math.abs(transform.a)
+
+    return [
+      {
+        name: "rect",
+        type: "element",
+        attributes: {
+          class: "pcb-solder-paste",
+          fill: solderPasteLayerNameToColor(solderPaste.layer),
+          x: (x - width / 2).toString(),
+          y: (y - height / 2).toString(),
+          width: width.toString(),
+          height: height.toString(),
+          rx: radius.toString(),
+        },
+      },
+    ]
+  }
+  // Implement circle-shaped SMT pad
+  if (solderPaste.shape === "circle") {
+    const radius = solderPaste.radius * Math.abs(transform.a)
+
+    return [
+      {
+        name: "circle",
+        type: "element",
+        attributes: {
+          class: "pcb-solder-paste",
+          fill: solderPasteLayerNameToColor(solderPaste.layer),
+          cx: x.toString(),
+          cy: y.toString(),
+          r: radius.toString(),
+        },
+      },
+    ]
+  }
+}
