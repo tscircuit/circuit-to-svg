fef39e3a647b62fd46f22b0da8acc925fc36a808
Shibo <shibosoftwaredev@gmail.com>
2025-05-18 19:00:33 +0200

    fixed type

--- lib/sch/svg-object-fns/create-svg-objects-for-sch-text.ts
+++ lib/sch/svg-object-fns/create-svg-objects-for-sch-text.ts
@@ -16,20 +16,66 @@ export const createSvgSchText = ({
   // Apply transformation
   const center = applyToPoint(transform, elm.position)
 
-  const textAnchorMap: Record<typeof elm.anchor, string> = {
+  const textAnchorMap: Record<
+    | "center"
+    | "left"
+    | "right"
+    | "top"
+    | "bottom"
+    | "top_left"
+    | "top_center"
+    | "top_right"
+    | "center_left"
+    | "center_right"
+    | "bottom_left"
+    | "bottom_center"
+    | "bottom_right",
+    string
+  > = {
     center: "middle",
     left: "start",
     right: "end",
     top: "middle",
     bottom: "middle",
+    top_left: "start",
+    top_center: "middle",
+    top_right: "end",
+    center_left: "start",
+    center_right: "end",
+    bottom_left: "start",
+    bottom_center: "middle",
+    bottom_right: "end",
   }
 
-  const dominantBaselineMap: Record<typeof elm.anchor, string> = {
+  const dominantBaselineMap: Record<
+    | "center"
+    | "left"
+    | "right"
+    | "top"
+    | "bottom"
+    | "top_left"
+    | "top_center"
+    | "top_right"
+    | "center_left"
+    | "center_right"
+    | "bottom_left"
+    | "bottom_center"
+    | "bottom_right",
+    string
+  > = {
     center: "middle",
     left: "middle",
     right: "middle",
     top: "hanging",
     bottom: "ideographic",
+    top_left: "hanging",
+    top_center: "hanging",
+    top_right: "hanging",
+    center_left: "middle",
+    center_right: "middle",
+    bottom_left: "ideographic",
+    bottom_center: "ideographic",
+    bottom_right: "ideographic",
   }
 
   return {
