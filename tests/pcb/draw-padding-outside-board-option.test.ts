51883f8e768a326009cf4b4385f926f6ae13eabe
Severin Ibarluzea <seve700@gmail.com>
2025-06-03 20:43:08 -0700

    Improve drawPaddingOutsideBoard test

--- tests/pcb/draw-padding-outside-board-option.test.ts
+++ tests/pcb/draw-padding-outside-board-option.test.ts
@@ -9,11 +9,31 @@ const circuit: any = [
     width: 10,
     height: 10,
   },
+  {
+    type: "pcb_smtpad",
+    pcb_smtpad_id: "pad0",
+    shape: "rect",
+    width: 1,
+    height: 1,
+    x: 0,
+    y: 0,
+  },
+  {
+    type: "pcb_via",
+    pcb_via_id: "via0",
+    x: 2,
+    y: 0,
+    hole_diameter: 0.5,
+    outer_diameter: 1,
+    layers: ["top", "bottom"],
+    from_layer: "top",
+    to_layer: "bottom",
+  },
 ]
 
-test("drawPaddingOutsideBoard false", () => {
+test("drawPaddingOutsideBoard false", async () => {
   const svg = convertCircuitJsonToPcbSvg(circuit, {
     drawPaddingOutsideBoard: false,
   })
-  expect(svg).toMatchSvgSnapshot(import.meta.path)
-})
+  await expect(svg).toMatchSvgSnapshot(import.meta.path)
+}, 10000)
