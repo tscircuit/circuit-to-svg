0051dd395fa61499ec7299e280717acd91f77193
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-06-26 04:14:16 +0530

    Address feedback for silkscreen text newline

--- tests/pcb/pcb-silkscreen-text-newline.test.ts
+++ tests/pcb/pcb-silkscreen-text-newline.test.ts
@@ -8,7 +8,7 @@ test("pcb silkscreen text newline", () => {
       layer: "top",
       pcb_silkscreen_text_id: "pcb_silkscreen_text_0",
       font: "tscircuit2024",
-      font_size: 1,
+      font_size: 0.2,
       pcb_component_id: "pcb_generic_component_0",
       anchor_position: { x: 0, y: 0 },
       anchor_alignment: "center",
