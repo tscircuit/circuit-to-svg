6a276049451d1f68500eeb66021f1911a99fefcf
Severin Ibarluzea <seve700@gmail.com>
2025-07-11 17:32:20 -0700

    feat: show not connected symbol

--- lib/sch/svg-object-fns/create-svg-objects-from-sch-port-on-box.ts
+++ lib/sch/svg-object-fns/create-svg-objects-from-sch-port-on-box.ts
@@ -9,19 +9,29 @@ import { su } from "@tscircuit/circuit-json-util"
 import { createSvgObjectsForSchPortBoxLine } from "./create-svg-objects-for-sch-port-box-line"
 import { createSvgObjectsForSchPortPinNumberText } from "./create-svg-objects-for-sch-port-pin-number-text"
 import { createSvgObjectsForSchPortPinLabel } from "./create-svg-objects-for-sch-port-pin-label"
+import type { ColorMap } from "lib/utils/colors"
+import { createSvgObjectsForNotConnectedSymbol } from "./create-svg-objects-for-not-connected-symbol"
 
 export const createSvgObjectsFromSchPortOnBox = (params: {
   schPort: SchematicPort
   schComponent: SchematicComponent
   transform: Matrix
   circuitJson: AnyCircuitElement[]
+  colorMap: ColorMap
 }): SvgObject[] => {
   const svgObjects: SvgObject[] = []
-  const { schPort, schComponent, transform, circuitJson } = params
+  const { schPort, schComponent, transform, circuitJson, colorMap } = params
 
   svgObjects.push(...createSvgObjectsForSchPortBoxLine(params))
   svgObjects.push(...createSvgObjectsForSchPortPinNumberText(params))
   svgObjects.push(...createSvgObjectsForSchPortPinLabel(params))
+  svgObjects.push(
+    ...createSvgObjectsForNotConnectedSymbol({
+      schPort,
+      transform,
+      colorMap,
+    }),
+  )
 
   return svgObjects
 }
