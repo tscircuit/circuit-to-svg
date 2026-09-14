01e82a5a62d9d3fb4f82702ef336c416c781ba86
StackedOndras <86679607+StackedOndras@users.noreply.github.com>
2026-09-14 02:47:19 +0200

    Exclude all hidden courtyard shapes from viewport bounds

--- lib/pcb/convert-circuit-json-to-pcb-svg.ts
+++ lib/pcb/convert-circuit-json-to-pcb-svg.ts
@@ -234,7 +234,13 @@ export function convertCircuitJsonToPcbSvg(
 
   const circuitJsonForBounds = options?.showCourtyards
     ? circuitJson
-    : circuitJson.filter((element) => element.type !== "pcb_courtyard_rect")
+    : circuitJson.filter(
+        (element) =>
+          element.type !== "pcb_courtyard_rect" &&
+          element.type !== "pcb_courtyard_circle" &&
+          element.type !== "pcb_courtyard_polygon" &&
+          element.type !== "pcb_courtyard_outline",
+      )
 
   const {
     minX,
