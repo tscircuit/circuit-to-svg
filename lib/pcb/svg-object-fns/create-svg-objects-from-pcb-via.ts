4671cfa7e9bb35b506a822621da8648b5ba3e98a
astinds <astinanto262@gmail.com>
2026-09-22 08:57:23 +0530

    fix(pcb): clamp negative via outer and inner radii to 0 (#640)

--- lib/pcb/svg-object-fns/create-svg-objects-from-pcb-via.ts
+++ lib/pcb/svg-object-fns/create-svg-objects-from-pcb-via.ts
@@ -31,8 +31,8 @@ export function createSvgObjectsFromPcbVia(
   const scaledHoleWidth = hole.hole_diameter * Math.abs(transform.a)
   const scaledHoleHeight = hole.hole_diameter * Math.abs(transform.a)
 
-  const outerRadius = Math.min(scaledOuterWidth, scaledOuterHeight) / 2
-  const innerRadius = Math.min(scaledHoleWidth, scaledHoleHeight) / 2
+  const outerRadius = Math.max(0, Math.min(scaledOuterWidth, scaledOuterHeight) / 2)
+  const innerRadius = Math.max(0, Math.min(scaledHoleWidth, scaledHoleHeight) / 2)
   const via: SvgObject = {
     name: "g",
     type: "element",
